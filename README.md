5aeee8affbc7f03bca59b1dfcd15cbfd07f3f029
aleviegas <alexandrev.barrosa@gmail.com>
2022-08-23 00:38:08 -0300

    Request API

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Node-consuming-API
 Project in node to consume and make a cashing database of the open weather API
+
---
+++ app.js
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const API_KEY = process.env.API_KEY
+const BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+// const res = await fetch('https://dog.ceo/api/breeds/list/all');
+// const json = await res.json();
+// console.log(res)
+// console.log(json);
+
+
+const capitals = [{city:"Porto Velho"}, {city:"bla bla bla"}]
+
+const responses = await Promise.all(capitals.map(async (capital) => {
+
+    const { city } = capital
+
+    const requestUrl = `${BASE_URL}?appid=${API_KEY}&q=${city}`
+    const response = await fetch(requestUrl)
+    const data = await response.json()
+    console.log(response.status)
+    
+    console.log(data)
+
+    if (response.status !== 200) {
+        return {city, message:data.message}
+    }
+
+    return {city, message:data.main}
+}))
+
+console.log(responses) 
